0e41b2512b3269bb97484b102ad269494d842a4f
!skhak <nurlanjunushaliev76@icloud.com>
2026-02-09 16:11:20 +0600

    cdcd

--- src/modules/auth/auth.controllers.ts
+++ src/modules/auth/auth.controllers.ts
@@ -4,23 +4,24 @@ import bcrypt from "bcrypt";
 import { generateToken } from "../../config/token";
 import { OAuth2Client } from "google-auth-library";
 
-// const getUser = async (req: Request, res: Response) => {
-//   try {
-//     const id = req.params.id;
-//     const user = await prisma.user.findUnique({
-//       where: { id },
-//     });
-//     res.status(200).json({
-//       user,
-//       success: true,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: error,
-//     });
-//   }
-// };
+const getUserAll = async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        notification: true,
+      },
+    });
+    res.status(200).json({
+      users,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
 
 const register = async (req: Request, res: Response) => {
   try {
@@ -181,5 +182,6 @@ const googleAuth = async (req: Request, res: Response) => {
 export default {
   register,
   login,
+  getUserAll,
   googleAuth,
 };
