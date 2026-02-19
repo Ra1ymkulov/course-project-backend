8dd0f2af54ce85091ddb663a71bc26ea7b5eb4d4
Ra1ymkulov <retype.uun@gmail.com>
2026-02-19 15:05:14 +0600

    dev

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRouter from "../modules/auth/auth.routes";
 import userRouter from "../modules/user/user.routes";
 import courseRouter from "../modules/course/course.routes";
+import chatRoutes from "../modules/chat/chat.routes";
 import notificationRouter from "../modules/notification/notification.routes";
 
 const routes = Router();
@@ -10,5 +11,6 @@ routes.use("/auth", authRouter);
 routes.use("/user", userRouter);
 routes.use("/course", courseRouter);
 routes.use("/notification", notificationRouter);
+routes.use("/chat", chatRoutes);
 
 export default routes;
